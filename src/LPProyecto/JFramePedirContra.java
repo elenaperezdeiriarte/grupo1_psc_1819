1a01cb0cd1ace7980d3ab196661e68c62bb1b084
Adrian Michelena <adrianmichelena@opendeusto.es>
2018-11-05 12:48:18 +0100

    Base De Datos

--- src/LPProyecto/JFramePedirContra.java
+++ src/LPProyecto/JFramePedirContra.java
@@ -3,9 +3,12 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import LDProyecto.BaseDatos;
+
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -15,7 +18,6 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 
 import LNProyecto.ClsBloqueo;
-import LNProyecto.ClsUnificadorDClases;
 
 import java.awt.Color;
 
@@ -27,7 +29,6 @@ public class JFramePedirContra extends JFrame implements ActionListener{
 	private JPasswordField passwordField;
 	private JButton btnVerificar;
 	private JLabel lblFaltan;
-	private ClsUnificadorDClases Gestor;
 	private JButton btnVolver;
 	public JFramePedirContra() {
 		
@@ -57,7 +58,7 @@ public class JFramePedirContra extends JFrame implements ActionListener{
 		
 		lblFaltan = new JLabel();
 		lblFaltan.setForeground(Color.RED);
-		lblFaltan.setBounds(82, 110, 257, 14);
+		lblFaltan.setBounds(82, 50, 257, 14);
 		contentPane.add(lblFaltan);
 		
 		btnVolver = new JButton(new ImageIcon(getClass().getResource("/images/back.png")));
@@ -67,9 +68,7 @@ public class JFramePedirContra extends JFrame implements ActionListener{
 		btnVolver.setBorderPainted(false);
 		btnVolver.addActionListener(this);
 		contentPane.add(btnVolver);
-		
-		Gestor = new ClsUnificadorDClases();
-		
+				
 		new ClsBloqueo();
 	}
 	
@@ -87,87 +86,127 @@ public class JFramePedirContra extends JFrame implements ActionListener{
 		}
 		
 		if (botonPulsado == btnVerificar)
-		{			
-			int semaforo = 0;
-			int candado = 0;
-			//Gestion de bloqueos
-			if(Gestor.intentosBloqueo()>=3)
+		{		
+//			int semaforo = 0;
+//			int candado = 0;
+//			
+//			//Gestion de bloqueos
+//			if(Gestor.intentosBloqueo()>=3)
+//			{
+//				semaforo = Gestor.semaforoBloqueo();
+//			}
+//			
+//			if (semaforo==0)
+//			{
+//				candado = 1;
+//				
+//				Gestor.sumarIntento();
+//				String contrasena1 = Gestor.leerContra();
+//				char[] contraescrita = passwordField.getPassword();
+//				
+//				if (contrasena1.equals(String.valueOf(contraescrita)))
+//				{
+//					Gestor.reiniciarIntentos();			
+//					JFrameMenuAdministrador objMenuAdministrador = new JFrameMenuAdministrador();
+//					objMenuAdministrador.setVisible(true);
+//					this.dispose();
+//				}
+//				else
+//				{
+//					lblFaltan.setText("                  Error en la contrasena");
+//					setContentPane(contentPane);
+//				    contentPane.validate();
+//				    contentPane.repaint();
+//										
+//					if(candado==1)
+//					{
+//						Gestor.sacarMinutos();
+//					}
+//						
+//					if(Gestor.getIntentos()>=3)
+//					{
+//						lblFaltan.setText("Espere " + (Gestor.getIntentos())  + " minuto(s) para poder acceder");
+//						setContentPane(contentPane);
+//					    contentPane.validate();
+//					    contentPane.repaint();
+//					}	
+//				}
+//					
+//				if(candado==1)
+//				{
+//					Gestor.sacarMinutos();
+//				}
+//			}
+//			else
+//			{
+//				long falta = Gestor.sacarFalta();
+//				if (falta<0)
+//				{
+//					TimerTask timerTask = new TimerTask()
+//				     {
+//						ClsUnificadorDClases Gestor = new ClsUnificadorDClases();
+//				         public void run() 
+//				         {
+//				        	lblFaltan.setText("Falta(n) " + (-Gestor.sacarFalta()) + " segundo(s) para poder acceder");
+//			        		
+//							setContentPane(contentPane);
+//							contentPane.validate();
+//							contentPane.repaint();
+//				         }
+//				     };
+//				      // Aqun se pone en marcha el timer cada segundo.
+//				     Timer timer = new Timer();
+//				     // Dentro de 0 milisegundos avnsame cada 1000 milisegundos
+//				     timer.scheduleAtFixedRate(timerTask, 0, 1000); 
+//				}
+//				else
+//				{
+//					lblFaltan.setText(null);
+//					
+//					setContentPane(contentPane);
+//				    contentPane.validate();
+//				    contentPane.repaint();
+//				}
+//			}
+			
+			
+			
+			
+			BaseDatos.initBD("eLibrary.db");
+
+			BaseDatos.crearTablaBDU();
+			
+			String savedContra = "";
+			
+			try 
 			{
-				semaforo = Gestor.semaforoBloqueo();
+				BaseDatos.devolverContra();
+			} 
+			catch (SQLException e1) 
+			{
+				e1.printStackTrace();
 			}
 			
-			if (semaforo==0)
+			savedContra = BaseDatos.getContra();
+			BaseDatos.close();
+			
+			if(savedContra.equals("")) {savedContra="a";}
+			
+			String writtenContra = new String(passwordField.getPassword());
+			Boolean comparation = false;
+			
+			
+			if(writtenContra.equals(savedContra)) {comparation = true;}
+			
+			System.out.println("Written: " + writtenContra + "\n" + "Saved: " + savedContra + "\n" + "Comparation: " + comparation);
+			
+			if(comparation==true)
 			{
-				candado = 1;
-				
-				Gestor.sumarIntento();
-				String contrasena1 = Gestor.leerContra();
-				char[] contraescrita = passwordField.getPassword();
-				
-				if (contrasena1.equals(String.valueOf(contraescrita)))
-				{
-					Gestor.reiniciarIntentos();			
-					JFrameMenuAdministrador objMenuAdministrador = new JFrameMenuAdministrador();
-					objMenuAdministrador.setVisible(true);
-					this.dispose();
-				}
-				else
-				{
-					lblFaltan.setText("                  Error en la contrasena");
-					setContentPane(contentPane);
-				    contentPane.validate();
-				    contentPane.repaint();
-										
-					if(candado==1)
-					{
-						Gestor.sacarMinutos();
-					}
-						
-					if(Gestor.getIntentos()>=3)
-					{
-						lblFaltan.setText("Espere " + (Gestor.getIntentos())  + " minuto(s) para poder acceder");
-						setContentPane(contentPane);
-					    contentPane.validate();
-					    contentPane.repaint();
-					}	
-				}
-					
-				if(candado==1)
-				{
-					Gestor.sacarMinutos();
-				}
+				JFrameMenuAdministrador objMenuAdministrador = new JFrameMenuAdministrador();
+				objMenuAdministrador.setVisible(true);
+				this.dispose();
 			}
-			else
-			{
-				long falta = Gestor.sacarFalta();
-				if (falta<0)
-				{
-					TimerTask timerTask = new TimerTask()
-				     {
-						ClsUnificadorDClases Gestor = new ClsUnificadorDClases();
-				         public void run() 
-				         {
-				        	lblFaltan.setText("Falta(n) " + (-Gestor.sacarFalta()) + " segundo(s) para poder acceder");
-			        		
-							setContentPane(contentPane);
-							contentPane.validate();
-							contentPane.repaint();
-				         }
-				     };
-				      // Aqun se pone en marcha el timer cada segundo.
-				     Timer timer = new Timer();
-				     // Dentro de 0 milisegundos avnsame cada 1000 milisegundos
-				     timer.scheduleAtFixedRate(timerTask, 0, 1000); 
-				}
-				else
-				{
-					lblFaltan.setText(null);
-					
-					setContentPane(contentPane);
-				    contentPane.validate();
-				    contentPane.repaint();
-				}
-			}
+			
 		}
 	}
 }
