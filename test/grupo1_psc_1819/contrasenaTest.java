c80d11da8db36b77a9ca4699d5aab5dc510118bc
lJulen <julengarro@opendeusto.es>
2018-11-06 10:40:21 +0100

    Carpeta de test

---
+++ test/grupo1_psc_1819/contrasenaTest.java
@@ -0,0 +1,23 @@
+package grupo1_psc_1819;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import LNProyecto.ClsContrasena;
+import LNProyecto.ClsUnificadorDClases;
+import LPProyecto.JFramePedirContra;
+
+
+public class contrasenaTest {
+
+	private JFramePedirContra contrasena = new JFramePedirContra();
+	private ClsUnificadorDClases Gestor = new ClsUnificadorDClases();
+	@Test
+	public void test() {
+		String password = Gestor.leerContra();
+			
+		assertTrue(!password.equals(""));
+	}
+
+}
