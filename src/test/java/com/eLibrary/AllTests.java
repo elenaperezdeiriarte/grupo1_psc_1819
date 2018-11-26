a49c5ded5dcbf36e9f7efef26af35b85de2adc59
lJulen <julengarro@opendeusto.es>
2018-11-26 10:17:58 +0100

    Mockito

--- src/test/java/com/eLibrary/AllTests.java
+++ src/test/java/com/eLibrary/AllTests.java
@@ -5,20 +5,24 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ adrianTest.class, 
-		elenaTest.class,
-		julenTest.class,
-		mikelTest.class
-		//articuloTest.class,
-		//cdTest.class, 
-		//contrasenaTest.class,
-		//dvdTest.class, 
-		//libroTest.class, 
-		//lpproyectoPerfTest.class,
-		//lpproyectoTest.class,
-		//quejasTest.class, 
-		//unificadorTest.class 
+@SuiteClasses({ 
+	adrianTest.class, 
+	elenaTest.class,
+	julenTest.class,
+	mikelTest.class,
+	cdMockTest.class
 	})
+	//articuloTest.class,
+	//cdTest.class, 
+	//contrasenaTest.class,
+	//dvdTest.class, 
+	//libroTest.class, 
+	//lpproyectoPerfTest.class,
+	//lpproyectoTest.class,
+	//quejasTest.class, 
+	//unificadorTest.class })
+
+
 public class AllTests {
 	
 
