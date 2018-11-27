b4c356a8f686e45b3cefe0919b50373aa95a92b6
Adrian Michelena <adrianmichelena@opendeusto.es>
2018-11-27 19:07:17 +0100

    PROYGRUPO1PSC1819 Ordenar ver estadísticas

--- src/main/java/LPProyecto/JFrameEstadisticas.java
+++ src/main/java/LPProyecto/JFrameEstadisticas.java
@@ -90,7 +90,8 @@ public class JFrameEstadisticas extends JFrame implements ActionListener
 		JScrollPane scrollPane_2 = new JScrollPane();
 		scrollPane_2.setBounds(918, 150, 400, 450);
 		contentPane.add(scrollPane_2);
-
+		
+		//ordenadr ver estadisticas
 		if (x == 1) {
 			btnNota = new JButton("Nota");
 			btnNota.setBounds(616, 648, 89, 23);
